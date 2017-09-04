109063bdbe51ab808fed23bee96989fac04233b1
AinulBedjo <ainul.y9b@gmail.com>
2017-09-04 20:23:42 +0700

    penambahan routes dan view engine.. && penambahan npm run dev di package.json

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const express = require('express')
 const app = epxress()
 const mongodb = require('mongodb')
 const mongoClient = mongodb.MongoClient
+const PORT = 2017
 
 // take this before handling server function.. !IMPORTANT
 app.use(bodyParser.urlencoded({
     extended: true
 }))
+
+app.use('/', express.static(__dirname + '/css'))
 
 // then connect to database
 mongoClient.connect('mongodb://ainul:insyaallah@ds151963.mlab.com:51963/checklist', (err, resutl) => {
@@ -16,4 +19,12 @@ mongoClient.connect('mongodb://ainul:insyaallah@ds151963.mlab.com:51963/checklis
     app.post('/', (req, res) => {
         console.log(req.body)
     })
+})
+
+app.get('/', (req, res) => {
+    res.render(__dirname + '/view/index.hbs')
+})
+
+app.listen(PORT, () => {
+    console.log('magic happen at port 2017')
 })
