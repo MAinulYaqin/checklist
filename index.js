15e185cfb858c61adc505322b09ba96cb72655b1
AinulBedjo <ainul.y9b@gmail.com>
2017-09-04 20:44:01 +0700

    :3 berhasil, tapi belum masuk ke database

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
 const join = require('path').join
+const mongodb = require('mongodb')
+const mongoClient = mongodb.MongoClient
 const PORT = 2017
 
 // take this before handling server function.. !IMPORTANT
@@ -10,7 +12,7 @@ app.use(bodyParser.urlencoded({
     extended: true
 }))
 
-// let db
+let db
 
 //setup view engine.. !IMPORTANT
 app.set('views', join(__dirname + 'view'))
@@ -20,12 +22,16 @@ app.set('view engine', 'hbs')
 app.use('/', express.static(__dirname + 'css'))
 
 // then connect to database
-// mongoClient.connect('mongodb://ainul:insyaallah@ds151963.mlab.com:51963/checklist', (err, database) => {
-//     if (err) throw err
+mongoClient.connect('mongodb://ainul2:Insyaallah@ds151963.mlab.com:51963/checklist', (err, database) => {
+    if (err) throw err
 
-//     db = database
-//     // yang ditaruh di naungan mongod adalah app.listen
-// })
+    db = database
+    
+    // yang ditaruh di naungan mongod adalah app.listen
+    app.listen(PORT, () => {
+        console.log('magic happen at port 2017')
+    })
+})
 
 app.get('/', (req, res) => {
     res.render(__dirname + '/view/index.hbs')
@@ -33,8 +39,4 @@ app.get('/', (req, res) => {
 
 app.post('/', (req, res) => {
     console.log(req.body)
-})
-
-app.listen(PORT, () => {
-    console.log('magic happen at port 2017')
 })
