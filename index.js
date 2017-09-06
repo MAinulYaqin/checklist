7909134e81c098cb03b39010d9aed40296d2d062
AinulBedjo <ainul.y9b@gmail.com>
2017-09-06 23:53:01 +0700

    modified index.js

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const PORT = 2017
 app.use(bodyParser.urlencoded({
     extended: true
 }))
+app.use(bodyParser.json())
 
 //setup view engine.. !IMPORTANT
 app.set('view engine', 'ejs')
@@ -18,16 +19,15 @@ app.set('views', join(__dirname + '/view'))
 
 // setup directory
 app.use('/', express.static(__dirname + '/css'))
+app.use('/', express.static(__dirname + '/js'))
 
 
 let db
 // then connect to database
 mongoClient.connect('mongodb://ainul:Insyaallah@ds151963.mlab.com:51963/checklist', (err, database) => {
     if (err) throw err
-
-
-    db = database
     // yang ditaruh di naungan mongod adalah app.listen
+    db = database
     app.listen(PORT, () => {
         console.log('magic happen at port 2017')
     })
@@ -36,7 +36,6 @@ mongoClient.connect('mongodb://ainul:Insyaallah@ds151963.mlab.com:51963/checklis
 app.get('/', (req, res) => {
     db.collection('checklist').find().toArray((err, result) => {
         if (err) throw err
-        
         console.log(result)
         res.render(__dirname + '/view/index.ejs', {
             quotes: result
@@ -48,9 +47,7 @@ app.get('/isi', (req, res) => {
     var cursor = db.collection('checklist').find().toArray((err, result) => {
         console.log(result)
     })
-
     console.log(cursor)
-
     res.send('wait until i get the problem').send(cursor)
 })
 
@@ -58,7 +55,39 @@ app.post('/quotes', (req, res) => {
     db.collection('checklist').save(req.body, (err, result) => {
         if (err) throw err
 
-        res.redirect('/')
+        // res.redirect('/')
         // console.log('save to database')
+    })
+})
+
+app.put('/quotes', (req, res) => {
+    db.collection('checklist').findOneAndUpdate({
+        name: 'Ainul'
+    }, {
+        $set: {
+            name: req.body.name,
+            option: req.body.option
+        }
+    }, {
+        sort: {
+            _id: -1
+        },
+        upsert: true
+    }, (err, result) => {
+        if (err) return res.send(err)
+        
+        console.log(result)
+    })
+})
+
+app.delete('/quotes', (req, res) => {
+    db.collection('checklist').findOneAndDelete({
+        name: req.body.name
+    }, (err, result) => {
+        if (err) throw err
+
+        res.send({
+            message: 'Deleted'
+        })
     })
 })
