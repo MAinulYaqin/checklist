438e9f107e55f3765b221f0304e3ea8baf7183ce
AinulBedjo <ainul.y9b@gmail.com>
2017-09-08 18:06:02 +0700

    adding update and delete, but delete doesn;t work

--- index.js
+++ index.js
@@ -11,32 +11,89 @@ const PORT = 2017
 app.use(bodyParser.urlencoded({
     extended: true
 }))
-
-let db
+app.use(bodyParser.json())
 
 //setup view engine.. !IMPORTANT
-app.set('views', join(__dirname + 'view'))
-app.set('view engine', 'hbs')
+app.set('view engine', 'ejs')
+app.set('views', join(__dirname + '/view'))
+
+// setup directory
+app.use('/', express.static(__dirname + '/css'))
+app.use('/', express.static(__dirname + '/js'))
 
 
-app.use('/', express.static(__dirname + 'css'))
-
+let db
 // then connect to database
-mongoClient.connect('mongodb://ainul2:Insyaallah@ds151963.mlab.com:51963/checklist', (err, database) => {
+mongoClient.connect('mongodb://ainul:Insyaallah@ds151963.mlab.com:51963/checklist', (err, database) => {
     if (err) throw err
-
-    db = database
-    
     // yang ditaruh di naungan mongod adalah app.listen
+    db = database
     app.listen(PORT, () => {
         console.log('magic happen at port 2017')
     })
 })
 
 app.get('/', (req, res) => {
-    res.render(__dirname + '/view/index.hbs')
+    db.collection('checklist').find().toArray((err, result) => {
+        if (err) throw err
+        console.log(result)
+        res.render(__dirname + '/view/index.ejs', {
+            quotes: result
+        })
+    })
 })
 
-app.post('/', (req, res) => {
-    console.log(req.body)
+app.get('/isi', (req, res) => {
+    var cursor = db.collection('checklist').find().toArray((err, result) => {
+        console.log(result)
+    })
+    console.log(cursor)
+    res.send('wait until i get the problem').send(cursor)
+})
+
+app.post('/quotes', (req, res) => {
+    db.collection('checklist').save(req.body, (err, result) => {
+        if (err) throw err
+
+        // res.redirect('/')
+        // console.log('save to database')
+    })
+})
+
+// Update, first params is query, then update, option, and callback
+app.put('/quotes', (req, res) => {
+    db.collection('checklist').findOneAndUpdate({
+        // query
+        name: 'Ainul'
+    }, {
+        // update
+        $set: {
+            name: req.body.name,
+            option: req.body.option
+        }
+    }, {
+        // option
+        sort: {
+            _id: -1
+        },
+        upsert: true
+    }, (err, result) => {
+        //callback
+        if (err) return res.send(err)
+        
+        console.log(result)
+    })
+})
+
+// delete doesn;t work, i dont know why
+app.delete('/quotes', (req, res) => {
+    db.collection('checklist').findOneAndDelete({
+        name: req.body.name
+    }, (err, result) => {
+        if (err) throw err
+
+        res.send({
+            message: 'Deleted'
+        })
+    })
 })
